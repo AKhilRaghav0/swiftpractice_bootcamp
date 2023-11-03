9c8c0a32c809fbc7c2fa626c19b0ee0d0975a096
Akhil Raghav <86706467+AKhilRaghav0@users.noreply.github.com>
2023-11-03 17:00:16 +0530

    Transition BootCamp
    Tried button with popup menu
    with minor blur

--- Shared/transitionBootcamp.swift
+++ Shared/transitionBootcamp.swift
@@ -15,7 +15,7 @@ struct transitionBootcamp: View {
     var body: some View {
         ZStack(alignment: .bottom){
             
-            LinearGradient(colors: [Color.cyan, Color.gray], startPoint: .leading, endPoint: .bottomTrailing)
+            LinearGradient(colors: [Color.red, Color.gray], startPoint: .leading, endPoint: .bottomTrailing)
                 .edgesIgnoringSafeArea(.all)
             
             VStack{
@@ -31,47 +31,55 @@ struct transitionBootcamp: View {
                     
                     RoundedRectangle(cornerRadius: 30)
                         .fill(Color.white.opacity(0.4))
-                        .blur(radius: 2)
+                        .blur(radius: 0.3)
                         .frame(height: UIScreen.main.bounds.height * 0.5) //will use 50% of a screen
 //                        .transition(.asymmetric(insertion: .move(edge: .leading), removal: .move(edge: .bottom)))
 //                        .transition(.move(edge: .bottom))
                         .transition(AnyTransition.opacity.animation(.easeInOut))
                         .animation(.spring())
-                    .shadow(radius: 20)
+                    .shadow(radius: 10)
                     
                     
                     
-                    HStack(alignment: .bottom){
-                    Spacer()
-                        Button(action: {
-                            // Add your button action here
-                        }) {
-                            Text("Just another label")
-                                .foregroundColor(.white)
-                                .frame(width: 150, height: 50)
-                                .background(
-                                    RoundedRectangle(cornerRadius: 10)
-                                        .fill(Color.red)
-                                )
-                        }
-
+                    VStack {
+                        Text("This is just a random text to check the button positions on my rounded button, it should be on the right positon if i add Spacer after that, lets see what will goona happen if i do that")
+                            .font(.system(size: 24))
+                            .fontWeight(.semibold)
+                            .foregroundColor(.white)
+                            .padding()
+                            
+                        
+                        HStack(alignment: .bottom){
                         Spacer()
-                        
-                        
-                        Button(action: {
-                            // Add your button action here
-                        }) {
-                            Text("Just a label")
-                                .foregroundColor(.white)
-                                .frame(width: 150, height: 50)
-                                .background(
-                                    RoundedRectangle(cornerRadius: 10)
-                                        .fill(Color.red)
-                                )
-                        }
-                        Spacer()
+                            Button(action: {
+                                // Add your button action here
+                            }) {
+                                Text("Just another label")
+                                    .bold()
+                                    .foregroundColor(.white)
+                                    .frame(maxWidth: 160, maxHeight: 70)
+                                    .background(
+                                        Color.gray.cornerRadius(10)
+                                    )
+                            }
 
-                        
+                            Spacer()
+                            
+                            
+                            Button(action: {
+                                // Add your button action here
+                            }) {
+                                Text("Just a label")
+                                    .bold()
+                                    .foregroundColor(.white)
+                                    .frame(width: 150, height: 50)
+                                    .background(Color.red.cornerRadius(10)
+                                    )
+                            }
+                            Spacer()
+
+                            
+                        }
                     }
                     
                 }
