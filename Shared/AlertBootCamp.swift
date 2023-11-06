fe391bc93f6a5e1af7e30fd286d01c56af84d5ad
Akhil Raghav <86706467+AKhilRaghav0@users.noreply.github.com>
2023-11-06 12:21:19 +0530

    Alerts with Multiple Statements
    hardcoded

--- Shared/AlertBootCamp.swift
+++ Shared/AlertBootCamp.swift
@@ -11,36 +11,57 @@ struct AlertBootCamp: View {
     
     
     @State var showAlert: Bool = false
+    @State var alertTitle: String = ""
+    @State var alertMessage: String =  ""
     @State var backgroundColor: Color = Color.yellow
     var body: some View {
         ZStack {
             backgroundColor
                 .edgesIgnoringSafeArea(.all)
-            Button("Click me"){
-                showAlert.toggle()
-            }
+            HStack {
+                Button("Button 1"){
+                    alertTitle = "Error uploading Video."
+                    alertMessage = "Your video is not supported by our containers"
+                    showAlert.toggle()
+                }
+                
+                Button("Button 2"){
+                    alertTitle = "Error in posting logs"
+                    alertMessage = "Your API is outdated"
+                    showAlert.toggle()
+                }
+                .accentColor(.red)
 
-            .shadow(radius: 20)
+                
+                .alert(isPresented: $showAlert, content:  {
+                   getAlert()
+            })
+            }
+            .shadow(color: .teal, radius: 20)
             .accentColor(.teal)
             .buttonStyle(.borderedProminent)
-            .alert(isPresented: $showAlert, content:  {
-                Alert(
-                    title: Text("IPhone Alert"),
-                    message: Text("Your iPhone 15 Pro is about to Blast."),
-                    primaryButton: .destructive(Text("Let's Die"), action: {
-                        backgroundColor  = .red
-                    }),
-                    secondaryButton: .cancel(Text("Run for Life")
-                                             , action: {
-                                                 backgroundColor = .gray
-                                             }))
-                //            Alert(title: Text("Just an error")
-                //                .foregroundColor(.red)
-                //                .fontWeight(.semibold))
-                
-                
-            })
         }
+    }
+    func getAlert() -> Alert {
+        return Alert(
+            title: Text(alertTitle),
+            message: Text(alertMessage),
+            dismissButton: .default(Text("Okayt")))
+//        return Alert(
+//            title: Text("IPhone Alert"),
+//            message: Text("Your iPhone 15 Pro is about to Blast."),
+//            primaryButton: .destructive(Text("Let's Die"), action: {
+//                backgroundColor  = .red
+//            }),
+//            secondaryButton: .cancel(Text("Run for Life")
+//                                     , action: {
+//                                         backgroundColor = .gray
+//                                     }))
+//        //            Alert(title: Text("Just an error")
+//        //                .foregroundColor(.red)
+//        //                .fontWeight(.semibold))
+//
+        
     }
 }
 
