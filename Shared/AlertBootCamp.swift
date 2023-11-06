5376686cfa0ec23b7f9dbda11045295e470f4e48
Akhil Raghav <86706467+AKhilRaghav0@users.noreply.github.com>
2023-11-06 11:27:55 +0530

    AlertBootCamp (Ui Updates)
    ButtonStyle
    Nested Background
    Button actoins
    Introduction of ( distructive, cancel, default)

--- Shared/AlertBootCamp.swift
+++ Shared/AlertBootCamp.swift
@@ -11,17 +11,36 @@ struct AlertBootCamp: View {
     
     
     @State var showAlert: Bool = false
+    @State var backgroundColor: Color = Color.yellow
     var body: some View {
-        Button("Click me"){
-            showAlert.toggle()
-        }
-        .alert(isPresented: $showAlert, content:  {
-            Alert(title: Text("Just an error")
-//                .foregroundColor(.red)
-                .fontWeight(.semibold))
+        ZStack {
+            backgroundColor
+                .edgesIgnoringSafeArea(.all)
+            Button("Click me"){
+                showAlert.toggle()
+            }
+
+            .shadow(radius: 20)
+            .accentColor(.teal)
+            .buttonStyle(.borderedProminent)
+            .alert(isPresented: $showAlert, content:  {
+                Alert(
+                    title: Text("IPhone Alert"),
+                    message: Text("Your iPhone 15 Pro is about to Blast."),
+                    primaryButton: .destructive(Text("Let's Die"), action: {
+                        backgroundColor  = .red
+                    }),
+                    secondaryButton: .cancel(Text("Run for Life")
+                                             , action: {
+                                                 backgroundColor = .gray
+                                             }))
+                //            Alert(title: Text("Just an error")
+                //                .foregroundColor(.red)
+                //                .fontWeight(.semibold))
                 
-            
-        })
+                
+            })
+        }
     }
 }
 
