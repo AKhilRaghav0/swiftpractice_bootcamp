0adad213b12d1d84272454f6b2ebbfbd1d832f87
Akhil Raghav <86706467+AKhilRaghav0@users.noreply.github.com>
2023-11-08 12:23:41 +0530

    datePicker Bootcamp
    dateFormatter
    Date Picker with Range

--- Shared/datePickerBootCamp.swift
+++ Shared/datePickerBootCamp.swift
@@ -8,6 +8,15 @@
 import SwiftUI
 
 struct datePickerBootCamp: View {
+    let startingDate: Date = Calendar.current.date(from: DateComponents(year: 2018)) ?? Date()
+    let endingDate: Date = Calendar.current.date(from: DateComponents(year: 2019)) ?? Date()
+    
+    var dateFormatter: DateFormatter {
+        let formatter = DateFormatter()
+//        formatter.dateStyle = .short
+        formatter.dateStyle = .medium
+        return formatter
+    }
     
     @State var selectedDate: Date = Date()
     
@@ -18,23 +27,34 @@ struct datePickerBootCamp: View {
             LinearGradient(colors: [Color.gray, Color.black], startPoint: .bottomTrailing, endPoint: .top)
                 .ignoresSafeArea(.all)
             
-            
-            DatePicker("Select a date: ", selection: $selectedDate, displayedComponents: [.date ])
-//            DatePicker(selection: $selectedDate,
-//                       label: {
-//                Text("Select a Date :")
-//
-//
-//                    .padding(.all)
-//
-//            })
-//            .datePickerStyle(.automatic)
-            .accentColor(.teal)
-            .foregroundColor(.black)
-            
-            .font(.headline)
-            .background(Color.white)
+            VStack {
+                
+                Text("Selected Date is: ")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                Text(dateFormatter.string(from: selectedDate))
+                    .font(.headline)
+                    .foregroundColor(.white)
+                DatePicker(selection: $selectedDate, in: startingDate...endingDate, displayedComponents: [.date], label: {
+                    Text("Select date")
+                })
+    //            DatePicker("Select a date: ", selection: $selectedDate, displayedComponents: [.date ])
+    //            DatePicker(selection: $selectedDate,
+    //                       label: {
+    //                Text("Select a Date :")
+    //
+    //
+    //                    .padding(.all)
+    //
+    //            })
+    //            .datePickerStyle(.automatic)
+                .accentColor(.teal)
+                .foregroundColor(.black)
+                
+                .font(.headline)
+                .background(Color.white)
             .shadow(radius: 10)
+            }
             
         }
     }
