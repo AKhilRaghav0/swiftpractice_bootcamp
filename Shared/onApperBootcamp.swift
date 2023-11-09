9e0c588e9f162203aa631b4040e899f98d13ae2e
Akhil Raghav <86706467+AKhilRaghav0@users.noreply.github.com>
2023-11-09 16:48:29 +0530

    onAppear Bootcamp
    added image on landscape
    COunter count inc on navigationtitle as we move down on lazyVStack
    ONAPPEAR Color + Text Change with 5 sec delay with a help of Dispatch Queue

---
+++ Shared/onApperBootcamp.swift
@@ -0,0 +1,77 @@
+//
+//  onApperBootcamp.swift
+//  BootCamp (iOS)
+//
+//  Created by Akhil on 09/11/23.
+//
+
+import SwiftUI
+
+struct onApperBootcamp: View {
+    
+    @State var myText: String = "My Text"
+    @State var backgroundColor: Color = Color.cyan
+    @State var onCount: Int = 0
+    
+    var body: some View {
+        
+        ZStack {
+            
+            
+            NavigationView {
+                ScrollView{
+                    Button(action: {
+                        onCount = 0
+                    }, label: {
+                        Text("Reset button")
+                            .font(.headline)
+//                            .foregroundColor(.red)
+                    })
+                    .accentColor(backgroundColor)
+                    .buttonStyle(.borderedProminent)
+                    LazyVStack{
+                        ForEach(0..<50){ _ in
+                            Image("landscape")
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(height: 200)
+                                .mask(RoundedRectangle(cornerRadius: 24)) // Use mask to clip the image
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 24)
+                                        .stroke(Color.white, lineWidth: 2) // Add a white stroke around the image
+                                )
+                            
+                                .padding()
+                                .cornerRadius(20)
+                            
+                            
+                                .onAppear{
+                                    onCount += 1
+                                }
+                            
+                            
+                            
+                        }
+                    }
+                }
+                .onAppear(perform: {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 5){
+                        myText = "This is the new Text"
+                        backgroundColor = .red
+                    }
+                    
+                })
+                .background(backgroundColor)
+                .navigationBarTitle("ON Appear, \(onCount)")
+            }
+            
+        }
+        //        .background(Color.black )
+    }
+}
+
+struct onApperBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        onApperBootcamp()
+    }
+}
