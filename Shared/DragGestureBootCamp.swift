eb7a587e9a63c680144be0ecda2785e1ffca9931
Akhil Raghav <86706467+AKhilRaghav0@users.noreply.github.com>
2023-11-17 23:21:18 +0530

    DragGestureBootcamp minor Changes

--- Shared/DragGestureBootCamp.swift
+++ Shared/DragGestureBootCamp.swift
@@ -11,36 +11,52 @@ struct DragGestureBootCamp: View {
     @State var offset: CGSize = .zero
     var body: some View {
         
-        RoundedRectangle(cornerRadius: 25)
-            .frame(width: 100, height: 100)
-            .shadow(radius: 10, x: 0.0, y: 4)
-            .overlay(
-                Image(systemName: "plus")
-                    .font(.largeTitle)
-                    .foregroundColor(.white)
-            )
-            .offset(offset)
-        
-            .gesture(
-                DragGesture()
-                    .onChanged{ value in
-                        withAnimation(.spring()) {
-                            offset = value.translation
+        ZStack {
+            
+            VStack {
+                Text("\(offset.width)")
+                Spacer()
+            }
+            
+            
+            RoundedRectangle(cornerRadius: 25)
+                .frame(width: 300, height: 500)
+                .shadow(radius: 10, x: 0.0, y: 4)
+                .overlay(
+                    Image(systemName: "plus")
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                )
+                .offset(offset)
+                .scaleEffect(getScaleAmount())
+            
+                .gesture(
+                    DragGesture()
+                        .onChanged{ value in
+                            withAnimation(.spring()) {
+                                offset = value.translation
+                            }
+                            
                         }
-                        
-                    }
-                    .onEnded { value in
-                        withAnimation(.spring()) {
-                            offset = .zero
+                        .onEnded { value in
+                            withAnimation(.spring()) {
+                                offset = .zero
+                            }
+                            
+                            
+                            
                         }
-                        
-                        
-                        
-                    }
-                
+                    
             )
+        }
         
         
+    }
+    func getScaleAmount() -> CGFloat {
+        let max  = UIScreen.main.bounds.width / 2
+        let currentAmount = abs(offset.width)
+        let percentage = currentAmount / max
+        return 1.0 - percentage
     }
 }
 
