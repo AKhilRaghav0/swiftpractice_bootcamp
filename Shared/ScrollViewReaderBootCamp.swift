3e5d96108643ded359cbf63865069e8752c931f2
Akhil Raghav <86706467+AKhilRaghav0@users.noreply.github.com>
2023-11-25 20:30:48 +0530

    Create ScrollViewReaderBootCamp.swift

---
+++ Shared/ScrollViewReaderBootCamp.swift
@@ -0,0 +1,35 @@
+//
+//  ScrollViewReaderBootCamp.swift
+//  BootCamp (iOS)
+//
+//  Created by Akhil on 25/11/23.
+//
+
+import SwiftUI
+
+struct ScrollViewReaderBootCamp: View {
+    var body: some View {
+        ScrollView {
+            
+            ScrollViewReader { proxy in
+                ForEach(0..<50) { index in
+                    Text("This is item #\(index)")
+                        .font(.headline)
+                        .frame(height: 200)
+                        .frame(maxWidth: .infinity)
+                        .background(Color.white)
+                        .cornerRadius(10)
+                        .shadow(radius: 10)
+                        .padding(10)
+                }
+            }
+            
+        }
+    }
+}
+
+struct ScrollViewReaderBootCamp_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrollViewReaderBootCamp()
+    }
+}
