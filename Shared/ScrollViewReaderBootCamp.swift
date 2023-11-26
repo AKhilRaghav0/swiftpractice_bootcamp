246cb7da419dd4b87e9b2bec193554b5bbc79b2c
Akhil Raghav <86706467+AKhilRaghav0@users.noreply.github.com>
2023-11-26 21:28:02 +0530

    Update ScrollViewReaderBootCamp.swift

--- Shared/ScrollViewReaderBootCamp.swift
+++ Shared/ScrollViewReaderBootCamp.swift
@@ -8,27 +8,66 @@
 import SwiftUI
 
 struct ScrollViewReaderBootCamp: View {
+    @State var scrollToInt: Int = 0
+    @State var textFieldText: String = ""
     var body: some View {
-        ScrollView {
+        VStack {
+            TextField("Enter your number here..", text: $textFieldText)
+                .padding(.horizontal)
+                .background(
+                    RoundedRectangle(cornerRadius: 14)
+                        .stroke(Color.gray, lineWidth: 3.0)
+                        .frame(height: 55)
+                )
+                .padding()
+                .keyboardType(.numberPad)
+            
             Button(action: {
-                
-            }, label: {
-                Text("Click here to goto animation #49")
-            })
-            
-            ScrollViewReader { proxy in
-                ForEach(0..<50) { index in
-                    Text("This is item #\(index)")
-                        .font(.headline)
-                        .frame(height: 200)
-                        .frame(maxWidth: .infinity)
-                        .background(Color.white)
-                        .cornerRadius(10)
-                        .shadow(radius: 10)
-                        .padding(10)
+                withAnimation(.spring() ) {
+                    if  let index = Int(textFieldText) {
+                        scrollToInt = index
+                    }
+                    
+//                    proxy.scrollTo(29, anchor: .top)
                 }
+            }, label: {
+                Text("Scroll Now ")
+                    .accentColor(.red)
+                    .padding()
+                    .background(
+                        Capsule()
+                            .stroke(Color.red, lineWidth: 4)
+                    )
+                    
+                    .padding(.horizontal)
+            })
+             
+            ScrollView {
+                ScrollViewReader { proxy in
+                    
+                   
+                    ForEach(0..<50) { index in
+                        Text("This is item #\(index)")
+                            .font(.headline)
+                            .frame(height: 200)
+                            .frame(maxWidth: .infinity)
+                            .background(Color.white)
+                            .cornerRadius(10)
+                            .shadow(radius: 10)
+                            .padding(10)
+                            .id(index)
+                    }
+                    .onChange(of: scrollToInt, perform: { value in
+                        withAnimation(.spring()) {
+                            proxy.scrollTo(value, anchor: .top)
+                        }
+                        
+                        
+                    })
+                }
+                
+                
             }
-            
         }
     }
 }
