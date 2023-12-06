cfc37e9f0ebfc213f57e424efea4e09211571dfc
Akhil Raghav <86706467+AKhilRaghav0@users.noreply.github.com>
2023-12-07 00:01:46 +0530

    Create LocalNotificationPushBootcamp.swift

---
+++ Shared/LocalNotificationPushBootcamp.swift
@@ -0,0 +1,31 @@
+//
+//  LocalNotificationPushBootcamp.swift
+//  BootCamp (iOS)
+//
+//  Created by Akhil on 06/12/23.
+//
+
+import SwiftUI
+import UserNotifications
+
+class NotificationManager {
+    
+    static let instance = NotificationManager()
+    
+    func NotificationAutharization() {
+        
+    }
+}
+
+struct LocalNotificationPushBootcamp: View {
+    
+    
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocalNotificationPushBootcamp()
+}
