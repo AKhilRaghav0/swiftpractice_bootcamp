3503e1cc68daab091a303afc60aac0ea81a58462
Akhil Raghav <86706467+AKhilRaghav0@users.noreply.github.com>
2023-12-06 22:22:22 +0530

    backdated commit test

--- Shared/LocalNotificationPushBootcamp.swift
+++ Shared/LocalNotificationPushBootcamp.swift
@@ -22,7 +22,7 @@ struct LocalNotificationPushBootcamp: View {
     
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text()
     }
 }
 
