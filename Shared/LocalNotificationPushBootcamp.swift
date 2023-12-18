6f4949cc010f4ca0275f904916f9fac67d9cbe82
Akhil Raghav <86706467+AKhilRaghav0@users.noreply.github.com>
2023-12-18 06:57:07 +0530

    Update LocalNotificationPushBootcamp.swift

--- Shared/LocalNotificationPushBootcamp.swift
+++ Shared/LocalNotificationPushBootcamp.swift
@@ -1,18 +1,62 @@
-//
-//  LocalNotificationPushBootcamp.swift
-//  BootCamp (iOS)
-//
-//  Created by Akhil on 06/12/23.
-//
-
 import SwiftUI
 import UserNotifications
+import CoreLocation
 
 class NotificationManager {
     
     static let instance = NotificationManager()
     
     func NotificationAutharization() {
+        let options: UNAuthorizationOptions = [.alert, .sound, .badge]
+        UNUserNotificationCenter.current().requestAuthorization(options: options) { (sucess, error) in
+            if let error = error {
+                print("Error: \(error)")
+            } else {
+                print("SUCESS")
+            }
+                
+        }
+        
+    }
+    
+    func scheduleNotification(){
+        let content = UNMutableNotificationContent()
+        content.title = "This is my first Notification"
+        content.subtitle = "This is content Inside! "
+        content.sound = .default
+        content.badge = 1
+        
+        //time
+//        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 10.0, repeats: false)
+        //calender
+//        var dateComponents = DateComponents()
+//        dateComponents.hour = 16
+//        dateComponents.minute = 25
+//
+//        
+//        
+//        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
+        //location
+        let coordinates = CLLocationCoordinate2D(
+            latitude: 40.00,
+            longitude: 50.00
+        )
+        let region = CLCircularRegion(
+            center: coordinates,
+            radius: 100,
+            identifier: UUID().uuidString)
+        region.notifyOnEntry = true
+        region.notifyOnExit = false
+        
+        let trigger = UNLocationNotificationTrigger(region: region, repeats: true)
+        
+        
+        
+        let request = UNNotificationRequest(
+            identifier: UUID().uuidString,
+            content: content,
+            trigger:  trigger)
+        UNUserNotificationCenter.current().add(request)
         
     }
 }
@@ -22,8 +66,20 @@ struct LocalNotificationPushBootcamp: View {
     
     
     var body: some View {
-        Text()
-    }
+        VStack(spacing: 25){
+            Button("Request Permission"){
+                NotificationManager.instance.NotificationAutharization()
+            }
+            Button("Schedule Notification") {
+                NotificationManager.instance.scheduleNotification()
+            }
+            .onAppear() {
+                UIApplication.shared.applicationIconBadgeNumber = 0
+            }
+            
+        }
+        
+}
 }
 
 #Preview {
